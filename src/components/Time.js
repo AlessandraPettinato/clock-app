1795bbe699ad5d9162935858b02a742649657d98
Alessandra Pettinato <alessandrapettinato@Alessandras-MacBook-Pro.local>
2021-05-10 19:48:01 +0200

    Move function more and relevant useState to app component

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,21 +1,9 @@
 import { useState } from "react";
 
-import TimeDetails from "./TimeDetails";
-
 export default function Time({ time }) {
-	const [more, setMore] = useState(false);
-
-	const handleClickMore = () => {
-		setMore(!more);
-	};
-
 	return (
 		<div>
 			<p>Current time is: {time.datetime}</p>
-			<>
-				<button onClick={handleClickMore}>{!more ? "More" : "Less"}</button>
-				{more && <TimeDetails time={time} />}
-			</>
 		</div>
 	);
 }
